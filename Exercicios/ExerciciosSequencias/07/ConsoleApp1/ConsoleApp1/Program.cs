a13056e7d9181caf1c00e253da9a8c414d9ebaf3
Victor Chaves <victor.chaves460@gmail.com>
2023-04-04 16:14:32 -0300

    ex 07

---
+++ Exercicios/ExerciciosSequencias/07/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿
+
+
+int codigoVendedor;
+double salarioFixo, comissao, totalVendas, salarioFinal;
+
+
+Console.Write("Digite o código do vendedor: ");
+codigoVendedor = int.Parse(Console.ReadLine());
+
+Console.Write("Informe o sálario fixo do vendedor: ");
+salarioFixo= double.Parse(Console.ReadLine());
+
+Console.Write("Informe o valor que foi vendido no mês: ");
+totalVendas= double.Parse(Console.ReadLine());
+
+Console.Write("Qual a porcentagem de comissão: ");
+comissao = double.Parse(Console.ReadLine());
+
+salarioFinal = (totalVendas * comissao / 100.00) + salarioFixo;
+
+
+Console.WriteLine($"\nVendedor: {codigoVendedor}, Sálario Final: {salarioFinal.ToString("F2")}");
+
+
