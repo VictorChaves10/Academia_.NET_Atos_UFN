d432fadfb36a49a46112e17ebe17b9f4c71ab129
Victor Chaves <victor.chaves460@gmail.com>
2023-04-04 20:53:49 -0300

    ex 09

---
+++ Exercicios/ExerciciosSequencias/09/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+
+
+
+Console.Write("informe o valor da mercadoria: ");
+
+double valorMercadoria = double.Parse(Console.ReadLine());
+
+
+double valorParcelas = valorMercadoria / 4;
+
+double valorEntrada = valorParcelas * 2;
+
+
+Console.WriteLine($"Valor entrada: $ {valorEntrada.ToString("F2")}");
+
+Console.WriteLine($"Valor parcelas: $ {valorParcelas.ToString("F2")}");
+
+   
+
