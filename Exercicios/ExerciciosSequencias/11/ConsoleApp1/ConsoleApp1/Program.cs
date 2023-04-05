a1b107bbceba655e85d3096388299ed050d4bbce
Victor Chaves <victor.chaves460@gmail.com>
2023-04-05 13:34:30 -0300

    ex11

---
+++ Exercicios/ExerciciosSequencias/11/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿
+
+int nulos, brancos, validos, mediaNulos, mediaBrancos, mediaValidos;
+
+Console.Write("Informe o número de eleitores: ");
+int eleitores = int.Parse(Console.ReadLine());
+
+
+Console.Write("Quantos votos foram nulos: ");
+nulos= int.Parse(Console.ReadLine());
+
+Console.Write("Quantos votos foram em brancos: ");
+brancos= int.Parse(Console.ReadLine());
+
+Console.Write("Quantos votos foram valídos: ");
+validos= int.Parse(Console.ReadLine());
+
+
+mediaNulos =  nulos * 100 / eleitores;
+
+mediaBrancos = brancos * 100 /  eleitores;
+
+mediaValidos =  validos * 100 / eleitores ;
+
+Console.WriteLine($"\nTotal de votos: Nulos {mediaNulos}%, Brancos {mediaBrancos}%, Válidos {mediaValidos}%");
+
+
+
+
+
+
+
