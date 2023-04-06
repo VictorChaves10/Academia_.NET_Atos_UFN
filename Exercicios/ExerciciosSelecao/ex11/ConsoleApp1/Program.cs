08028e1c62cdc05fb1b229ef372465e8058a2a9a
Victor Chaves <victor.chaves460@gmail.com>
2023-04-06 18:10:20 -0300

    ex11

---
+++ Exercicios/ExerciciosSelecao/ex11/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+
+Console.Write("Informe as horas: ");
+string[] horario = Console.ReadLine().Split(":");
+
+int hora = int.Parse(horario[0]);
+int minutos = int.Parse(horario[1]);
+
+if(hora < 24 && hora >= 0)
+{
+    Console.WriteLine("Hora valida!");
+}
+else
+{
+    Console.WriteLine("Hora inválida!");
+}
