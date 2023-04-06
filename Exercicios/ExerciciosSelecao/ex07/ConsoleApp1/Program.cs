17fea4a457de8830e87e42ae59f07c3a5512b541
Victor Chaves <victor.chaves460@gmail.com>
2023-04-06 15:35:00 -0300

    ex07

---
+++ Exercicios/ExerciciosSelecao/ex07/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string frase, pesquisa;
+            Console.Write("Escreva uma frase: ");
+            frase = Console.ReadLine();
+
+            Console.Write("Digite a palarava de pesquisa: ");
+            pesquisa = Console.ReadLine();
+
+            if (frase.Contains(pesquisa))
+            {
+                Console.WriteLine("A palavra '" + pesquisa+"', contem na frase.");
+            }
+            else
+            {
+                Console.WriteLine("A palavra '" + pesquisa+"', não contem na frase.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
