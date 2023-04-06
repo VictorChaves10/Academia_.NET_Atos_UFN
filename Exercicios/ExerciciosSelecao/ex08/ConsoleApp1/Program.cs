ddcf992c56141f759b6784a4e9dde7309757f6d6
Victor Chaves <victor.chaves460@gmail.com>
2023-04-06 16:15:52 -0300

    ex08

---
+++ Exercicios/ExerciciosSelecao/ex08/ConsoleApp1/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int codigo;
+            string nome;
+            double salario,totalVendas;
+
+            Console.Write("Informe o seu código: ");
+            codigo = int.Parse(Console.ReadLine());
+
+            Console.Write("\nInforme seu nome: ");
+            nome = Console.ReadLine();
+
+            Console.Write("\nInforme o seu salário base: ");
+            salario = double.Parse(Console.ReadLine());
+
+            Console.Write("\nInforme o total de vendas: ");
+            totalVendas = double.Parse(Console.ReadLine());
+
+
+            if(totalVendas > 500.00 && totalVendas <= 1000.00)
+            {
+                double salarioFinal = (totalVendas * 5.00 / 100.00) + salario;
+                Console.WriteLine($"Nome: {nome}, código: {codigo}, salário inicial: {salario.ToString("f2")}, salário + comissão: {salarioFinal.ToString("F2")} ");
+            }
+            else if(totalVendas > 1000.00 && totalVendas <= 5000.00)
+            {
+                double salarioFinal = (totalVendas * 7.00 / 100.00) + salario;
+                Console.WriteLine($"Nome: {nome}, código: {codigo}, salário inicial: {salario.ToString("f2")}, salário + comissão: {salarioFinal.ToString("F2")} ");
+            }
+            else if (totalVendas > 5000.00)
+            {
+                double salarioFinal = (totalVendas * 10.00 / 100.00) + salario;
+                Console.WriteLine($"Nome: {nome}, código: {codigo}, salário inicial: {salario.ToString("f2")}, salário + comissão: {salarioFinal.ToString("F2")} ");
+            }
+            else
+            {
+                
+                Console.WriteLine($"Nome: {nome}, código: {codigo}, salário inicial: {salario.ToString("f2")}, salário + comissão: {salario.ToString("F2")} ");
+            }
+        }
+    }
+}
