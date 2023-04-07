f925ad350d195fdcde801d7050ab812143c1fef1
Victor Chaves <victor.chaves460@gmail.com>
2023-04-07 11:12:02 -0300

    ex12

---
+++ Exercicios/ExerciciosSelecao/ex12/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿
+
+int codig;
+double salary = 0.00, extra =00.00;
+
+Console.Write("Enter your code: ");
+codig = int.Parse(Console.ReadLine());
+
+Console.Write("How many hours you worked: ");
+string[] numberHours = Console.ReadLine().Split(":");
+
+int hours = int.Parse(numberHours[0]);
+int minutes = int.Parse(numberHours[1]);
+
+salary = (hours * 10.00)+(minutes * 0.17);
+
+if(hours >= 50 && minutes > 0)
+{
+    extra = ((hours - 50) * 20.00) + (minutes * 0.34);
+    salary += extra;
+    
+}
+
+Console.WriteLine($"Código: {codig}, salário total: R$ {salary.ToString("F2")}, salário extra: R$ {extra.ToString("F2")}");
