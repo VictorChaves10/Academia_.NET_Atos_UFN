7630a6c6ad7ff6a081a5f2759078f9da008dc391
Victor Chaves <victor.chaves460@gmail.com>
2023-04-07 11:50:00 -0300

    ex13

---
+++ Exercicios/ExerciciosSelecao/ex13/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+double height, ideaWeight;
+char genre;
+Console.Write("Enter your Height: ");
+height = double.Parse(Console.ReadLine());
+
+Console.WriteLine("\nEnter your Genre ('M' or 'W'): ");
+genre = char.Parse(Console.ReadLine());
+
+
+if(genre == 'm' || genre == 'M')
+{
+    ideaWeight = (72.7 * height) - 58;
+    Console.WriteLine("Your ideal weight is: "+ ideaWeight.ToString("F1"));
+}
+else if(genre == 'w' || genre == 'W')
+{
+    ideaWeight = (62.1 * height) - 44.7;
+    Console.WriteLine();
+    Console.WriteLine("Your ideal weight is: " + ideaWeight.ToString("F1"));
+}
