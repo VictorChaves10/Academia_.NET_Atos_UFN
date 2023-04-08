93ef4649ad5224faabf59edf8c1b4ad245a207eb
Victor Chaves <victor.chaves460@gmail.com>
2023-04-07 22:59:23 -0300

    ex14

---
+++ Exercicios/ExerciciosSelecao/ex15/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+
+
+
+double metrosQuadrados;
+int qtdLatas;
+double precoTotal;
+
+Console.Write("Informe a área a ser pintada em metros quadrados: ");
+metrosQuadrados = double.Parse(Console.ReadLine());
+
+qtdLatas = (int)Math.Ceiling(metrosQuadrados / 54);
+
+precoTotal = qtdLatas * 80;
+
+Console.WriteLine("Você precisará de " + qtdLatas + " latas de tinta, ao custo total de R$" + precoTotal.ToString("F2"));
+
