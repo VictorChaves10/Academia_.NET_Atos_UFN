28cdd02711859c8d7e58e19ff13adb2a892680fd
Victor Chaves <victor.chaves460@gmail.com>
2023-04-08 09:33:12 -0300

    ex17

---
+++ Exercicios/ExerciciosSelecao/ex17/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿
+
+double l1, l2, l3;
+Console.WriteLine("Informe o tamanho dos lados do triângulo: ");
+l1 = double.Parse(Console.ReadLine());
+l2 = double.Parse(Console.ReadLine());
+l3 = double.Parse(Console.ReadLine());
+
+if (l1 + l2 >= l3 && l3 + l1 >= l2 && l2 + l3 >= l1)
+{
+    if (l1 == l2 && l2 == l3)
+    {
+        Console.WriteLine("Triângulo equilátero");
+    }
+    else if (l1 == l2 || l2 == l3 || l1 == l3)
+    {
+
+        Console.WriteLine("Triângulo isoceles");
+    }
+    else
+    {
+        Console.WriteLine("Triângulo escaleno");
+    }
+}
+else
+{
+    Console.WriteLine("Não é um trîângulo");
+}
+
+
