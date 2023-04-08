15790c83d4b67639369d522b6d8e8cacde611ea2
Victor Chaves <victor.chaves460@gmail.com>
2023-04-07 23:17:31 -0300

    ex16

---
+++ Exercicios/ExerciciosSelecao/ex16/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿
+
+
+
+Console.Write("Informe o placar do jogo: ");
+string[] placar = Console.ReadLine().Split('x');
+int house = int.Parse(placar[0]);
+int fora = int.Parse(placar[1]);
+
+if ((house - fora) >= 2)
+{
+    Console.WriteLine("Os dois times irão se enfrentar novamente em um novo jogo");
+    Console.Write("\nInforme o placar do jogo: ");
+     placar = Console.ReadLine().Split('x');
+     house = int.Parse(placar[0]);
+     fora = int.Parse(placar[1]);
+    if(house > fora)
+    {
+        Console.WriteLine("O time da casa se classificou!");
+    }
+    else
+    {
+        Console.WriteLine("O time de fora se classificou");
+    }
+}
+else
+{
+    Console.WriteLine("O time de fora já se classificou");
+
+}
