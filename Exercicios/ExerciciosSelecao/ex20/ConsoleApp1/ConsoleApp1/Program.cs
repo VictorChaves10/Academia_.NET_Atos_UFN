38427579c712a211b248e377f47011315aa73c23
Victor Chaves <victor.chaves460@gmail.com>
2023-04-08 10:29:20 -0300

    ex20

---
+++ Exercicios/ExerciciosSelecao/ex20/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿
+
+Random random = new Random();
+
+int chance = 3;
+int numberRandom = random.Next(10);
+
+Console.Write("Informe sua primeira tentativa: ");
+int palpite = int.Parse(Console.ReadLine());
+
+while(palpite != numberRandom && chance != 1)
+{
+    chance--;
+
+    if (palpite > numberRandom)
+    {
+        Console.WriteLine("É menor!");
+    }
+    else
+    {
+        Console.WriteLine("É maior!");
+    }
+
+    Console.Write($"Você tem mais {chance} tentativas: ");
+    palpite = int.Parse(Console.ReadLine());
+}
+
+if (palpite == numberRandom)
+{
+    Console.WriteLine("Você acertou");
+}
+else
+{
+    Console.WriteLine("Suas chances acabaram!");
+}
