934d2bafe71ab6d5b64e3a98a9eabc8e66d8e2ba
Victor Chaves <victor.chaves460@gmail.com>
2023-04-08 10:00:02 -0300

    ex19

---
+++ Exercicios/ExerciciosSelecao/ex19/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,62 @@
+﻿
+
+
+
+double n1, n2, n3;
+
+Console.WriteLine("Informe os valores:");
+n1 = double.Parse(Console.ReadLine());
+n2 = double.Parse(Console.ReadLine());
+n3 = double.Parse(Console.ReadLine());
+
+
+if(n1 > n2 && n1 > n3)
+{
+    Console.WriteLine(n1);
+    if(n2 > n3)
+    {
+        Console.WriteLine(n2);
+        Console.WriteLine(n3);
+    }
+    else
+    {
+        Console.WriteLine(n3);
+        Console.WriteLine(n2);
+    }
+}
+else if (n2 > n3 && n2 > n1)
+{
+    Console.WriteLine(n2);
+    if(n1 > n3)
+    {
+        Console.WriteLine(n1);
+        Console.WriteLine(n3);
+    }
+    else
+    {
+        Console.WriteLine(n3);
+        Console.WriteLine(n1);
+    }
+}
+else
+{
+    Console.WriteLine(n3);
+    if (n1 > n2)
+    {
+        Console.WriteLine(n1);
+        Console.WriteLine(n2);
+    }
+}
+
+if (n1 > n2 && n1 > n3)
+{
+    Console.WriteLine($"\nO {n1} é o maior!");
+}
+else if (n2 > n3)
+{
+    Console.WriteLine($"\nO {n2} é o maior!");
+}
+else
+{
+    Console.WriteLine($"\nO {n3} é o maior!");
+}
