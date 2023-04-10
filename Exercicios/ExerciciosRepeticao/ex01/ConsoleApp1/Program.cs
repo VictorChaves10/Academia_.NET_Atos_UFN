4c040c5202bfcb0804c6b4c869f7476b724ba5dc
Victor Chaves <victor.chaves460@gmail.com>
2023-04-10 16:57:01 -0300

    ex01

---
+++ Exercicios/ExerciciosRepeticao/ex01/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+////1 - Escreva um algoritmo para ler um número n, e em seguida ler pelo teclado a idade de n 
+//pessoas e calcular a média das idades.
+
+
+Console.Write("Informe a quantidade de pessoas que será informado a idade: ");
+int x = int.Parse(Console.ReadLine());
+double media = 0;
+
+
+for(int i = 0; i < x; i++)
+{
+    int idade = int.Parse(Console.ReadLine());
+    media += idade;
+}
+
+Console.WriteLine(media / x);
+
