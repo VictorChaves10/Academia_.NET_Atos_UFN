73e3af68d00b0cc9099b76db1d19e9ec2517f451
Victor Chaves <victor.chaves460@gmail.com>
2023-04-11 14:37:19 -0300

    ex03

---
+++ Exercicios/ExerciciosRepeticao/ex03/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+
+
+
+int resultado = 0;
+Console.Write("Informe a tabuada do número desejado: ");
+int tabuada = int.Parse(Console.ReadLine());
+
+for (int i = 0; i <= 10; i++)
+{
+    resultado = tabuada * i;
+    Console.WriteLine($"{tabuada} x {i} = {resultado}");
+}
+
+
+
+/*3 - Escreva um algoritmo que leia pelo teclado um valor e então calcule a tabuada de 1 a 10 de um número
+x lido utilizando laços de repetição, e mostre na tela.*/
+
+
+
