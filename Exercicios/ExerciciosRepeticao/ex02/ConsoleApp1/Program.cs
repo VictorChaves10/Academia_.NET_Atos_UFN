9bfdf73e2bdfaf98d9a81fd5f262904937737226
Victor Chaves <victor.chaves460@gmail.com>
2023-04-11 14:21:40 -0300

    ex02

---
+++ Exercicios/ExerciciosRepeticao/ex02/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿
+
+
+
+
+int i, n, maior;
+Console.WriteLine("Digite um valor: ");
+maior = int.Parse(Console.ReadLine());
+
+for (i = 0; i < 9; i++)
+{
+    Console.WriteLine("Digite um valor: ");
+    n = int.Parse(Console.ReadLine());
+
+    if (n > maior) { 
+        maior = n; 
+    }
+}
+
+Console.WriteLine("O maior valor digitado foi: " + maior);
+
+
+
+
+
+
+
