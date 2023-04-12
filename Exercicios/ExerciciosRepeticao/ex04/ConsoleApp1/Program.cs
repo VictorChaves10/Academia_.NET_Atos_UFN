3b387a65a1cee2fe44505b91c703cec42bbde3ba
Victor Chaves <victor.chaves460@gmail.com>
2023-04-12 14:45:41 -0300

    ex05

---
+++ Exercicios/ExerciciosRepeticao/ex04/ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿//Escreva um algoritmo que leia dois valores pelo teclado, x e y, e em seguida calcule a potência de x elevado na y sem utilizar a função pow.
+
+int x, y, potencia, n;
+
+Console.Write("Informe o valor de x: ");
+x = int.Parse(Console.ReadLine());
+
+Console.Write("Informe o valor de y: ");
+y = int.Parse(Console.ReadLine());
+
+if (x > 1)
+{
+    n = x * x;
+
+    for (int i = 2; i < y; i++)
+    {
+
+        potencia = n * x;
+        n = potencia;
+
+    }
+
+    Console.WriteLine(n);
+    Console.WriteLine(Math.Pow(x, y));
+}
+else
+{
+    Console.WriteLine(x);
+}
---
+++ Exercicios/ExerciciosRepeticao/ex05/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+//Escreva um algoritmo para calcular o fatorial de um número.
+
+
+Console.Write("Informe qual número deseja saber os fatorias: ");
+int n = int.Parse(Console.ReadLine());
+
+if (n != 0)
+{
+    for (int i = n - 1; i != 0; i--)
+    {
+        n = n * i;
+
+    }
+
+    Console.WriteLine(n);
+}
+else
+{
+    Console.WriteLine('1');
+}
+
