18913e9793d39b795b4c3eb67ea1cae00f02c237
Victor Chaves <victor.chaves460@gmail.com>
2023-04-12 15:42:07 -0300

    ex01

---
+++ Exercicios/ExerciciosRepeticao02/ex01/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿//1.Faça um programa em VS que solicite um número inteiro positivo ao usuário, validando a entrada de dados (informando se ele estiver errado e repetindo a solicitação até que esteja correto). 
+//Após o programa em VS deve informar todos os números pares existentes entre 1 e o número fornecido pelo usuário.
+
+using System.Drawing;
+
+Console.Write("Informe um número inteiro e positivo: ");
+int x = int.Parse(Console.ReadLine());
+
+while (x < 0)
+{
+    Console.Write("Informe um número inteiro e positivo: ");
+    x = int.Parse(Console.ReadLine());
+}
+
+for (int i = 1; i < x; i++)
+{
+    if (i % 2 == 0)
+    {
+        Console.Write(i + ", ");
+    }
+}
