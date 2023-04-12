4938ec31cd7b52c70480d088044c0dab583b4b33
Victor Chaves <victor.chaves460@gmail.com>
2023-04-12 15:16:27 -0300

    ex07

---
+++ Exercicios/ExerciciosRepeticao/ex07/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿//7.Escreva um programa em C# leia 20 números e que conte a quantidade de números pares e ímpares digitados por 
+//um usuário. Apresente o resultado.
+
+int n, i, par = 0, impar = 0, zero = 0;
+for (i = 0; i < 20; i++)
+{
+    Console.WriteLine("Digite um número: ");
+    n = int.Parse(Console.ReadLine());
+    if (n == 0)
+    {
+        zero++;
+    }
+    else if (n % 2 == 0)
+    {
+        par++;
+    }
+    else
+    {
+        impar++;
+    }
+}
+Console.WriteLine("Foram digitados " + par + " pares");
+Console.WriteLine("E foram digitados " + impar + " ímpares");
+Console.WriteLine("E foram digitados " + zero + " zeros");
+        
