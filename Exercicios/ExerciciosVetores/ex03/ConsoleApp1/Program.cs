75fd1476d9597c979ab59d24a72b59cd0196cfb4
Victor Chaves <victor.chaves460@gmail.com>
2023-04-13 16:38:40 -0300

    exercicios,3,4

---
+++ Exercicios/ExerciciosVetores/ex03/ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿//Escreva um algoritmo que leia os valores para um vetor de 20 elementos e então mostre na tela o índice da posição dos valores correspondentes a números primos.
+
+
+int[] vet = new int[20];
+Random valorVet= new Random();
+
+
+for (int i = 0;i < 20; i++)
+{
+    Console.Write($"Digite o valor na posição {i + 1} do vetor: ");
+    vet[i] = valorVet.Next(1,100);
+    Console.WriteLine(vet[i]);
+    
+}
+Console.WriteLine();
+for(int i = 0; i < 20; i++) {
+
+    int primo = 0;
+
+    for (int x = 1; x <= vet[i]; x++){
+        
+        if (vet[i] % x == 0)
+        {
+            primo++;
+        }
+    
+    }
+
+    if (primo > 2)
+    {
+        Console.WriteLine($"O valor {vet[i]} na posição {i+1} não é primo");
+    }
+    else
+    {
+        Console.WriteLine($"O valor {vet[i]} na posição {i+1} é primo");
+    }
+
+}
---
+++ Exercicios/ExerciciosVetores/ex04/ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿//Escreva um algoritmo que leia dois vetores de 10 posições e faça a multiplicação dos elementos de mesmo índice, colocando o resultado em um terceiro vetor. Mostre o vetor resultante.
+
+int[] vet1 = new int[10], vet2 = new int[10], vetResult = new int[10];
+
+for (int i = 0; i < 10; i++)
+{
+    Random valorVetor = new Random();
+    Console.Write($"Digite o valor para o vetor 1 na posição {i + 1}: ");
+    vet1[i] = valorVetor.Next(50);
+    Console.WriteLine(vet1[i]);
+
+    Console.Write($"Digite o valor para o vetor 2 na posição {i + 1}: ");
+    vet2[i] = valorVetor.Next(50);
+    vetResult[i] = vet1[i] * vet2[i];
+    Console.WriteLine(vet2[i]);
+
+    Console.WriteLine($"\nO resultado da multiplicação dos vetores é: {vetResult[i]}\n");
+}
