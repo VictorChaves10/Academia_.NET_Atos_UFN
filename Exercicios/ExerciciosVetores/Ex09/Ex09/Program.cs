4f75fd0c39697cdd3db6d204b46c88d10983cbe3
Victor Chaves <victor.chaves460@gmail.com>
2023-04-15 14:10:49 -0300

    Ex09

---
+++ Exercicios/ExerciciosVetores/Ex09/Ex09/Program.cs
@@ -0,0 +1,36 @@
+﻿
+class Program
+{
+    static void Main(string[] args) { 
+
+        int[] vetor = new int[10];
+        Console.WriteLine("Digite 10 valores para o vetor:");
+        for (int i = 0; i < 10; i++)
+        {
+            Console.Write($"Valor {i + 1}: ");
+            vetor[i] = int.Parse(Console.ReadLine());
+        }
+
+        for (int i = 0; i < 9; i++)
+        {
+            int menor = i;
+
+            for (int j = i + 1; j < 10; j++)
+            {
+                if (vetor[j] < vetor[menor])
+                {
+                    menor = j;
+                }
+            }
+            int temp = vetor[i];
+            vetor[i] = vetor[menor];
+            vetor[menor] = temp;
+        }
+
+        Console.WriteLine("\nVetor ordenado em ordem crescente:");
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine($"Valor {i + 1}: {vetor[i]}");
+        }
+    }
+}
