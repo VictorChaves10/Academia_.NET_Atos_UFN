6eb0f76eff30249f96e9f2620995e53707391f68
Victor Chaves <victor.chaves460@gmail.com>
2023-04-15 14:59:33 -0300

    Ex11

---
+++ Exercicios/ExerciciosVetores/Ex11/Ex11/Program.cs
@@ -0,0 +1,23 @@
+﻿//Escreva um programa que leia valores em um vetor de 5 posições. Escrever os elementos do vetor e após escrever os elementos na ordem inversa.
+
+int[] vect = new int[5];
+
+for(int i= 0; i < 5; i++)
+{
+    Console.Write($"Digite o valor na posição {i+1}: ");
+    vect[i] = int.Parse(Console.ReadLine());
+}
+
+Console.Write("\nEsses são os valores ordenados da lista: ");
+
+for (int i = 0; i < 5; i++)
+{
+    Console.Write($"{vect[i]} ");
+}
+
+Console.Write("\nEsses são os valores inversos da lista: ");
+
+for (int i = 4; i >= 0; i--)
+{
+    Console.Write($"{vect[i]} ");
+}
