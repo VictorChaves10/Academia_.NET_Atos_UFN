2198f02da418fe5e8914ec1afba2d110e284acc7
Victor Chaves <victor.chaves460@gmail.com>
2023-04-16 21:13:28 -0300

    ex02

---
+++ Exercicios/ExerciciosMatrizes/Ex02/Ex02/Program.cs
@@ -0,0 +1,36 @@
+﻿//2Solicite ao usuário, preencher uma Matriz 3x3
+
+//Informe ao usuário:
+//*A soma dos elementos de cada linha
+//	-Ex: Linha 1: 30
+
+//		 Linha 2: 17
+//* A soma dos elementos de cada coluna
+//	-Ex: Coluna 1: 23
+
+//		 Coluna 2: 36
+
+int[,] mat = new int[3,3];
+
+for(int i =0; i < 3;i++)
+{
+    for(int j = 0; j < 3; j++)
+    {
+        Console.Write($"Informe o valor da coluna {i+1} e linha {j+1}: ");
+        mat[j, i] = int.Parse(Console.ReadLine());
+        
+    }
+    Console.WriteLine();
+}
+
+
+for(int i =0; i < 3; i++)
+{
+    int temp = 0;
+    for(int j =1; j < 4; j++)
+    {       
+        int soma = mat[j, i] + temp;
+        temp = soma;
+    }
+    
+}
