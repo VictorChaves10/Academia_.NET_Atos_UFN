02f6d4605f83216843f554e74fa28519a82e2129
Victor Chaves <victor.chaves460@gmail.com>
2023-04-17 15:49:03 -0300

    Ex08

---
+++ Exercicios/ExerciciosMatrizes/Ex08/Program.cs
@@ -0,0 +1,39 @@
+﻿
+// Ler uma matriz com 4x4 de inteiros e mostrar os números na ordem direta e inversa a que foram lidos.
+
+
+
+int[,] mat = new int[4, 4];
+Random valueMat = new Random();
+
+for(int i = 0; i< 4; i++)
+{
+    for (int j = 0; j < 4; j++) 
+    {
+        mat[j, i] = valueMat.Next(0, 20);
+    }
+}
+
+Console.WriteLine("Matriz ordenada\n");
+
+for (int i = 0; i < 4; i++)
+{
+    for (int j = 0; j < 4; j++)
+    {
+        Console.Write($"{mat[j, i]}\t");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine("\nMatriz inversa\n");
+
+for (int i = 3; i >= 0; i--)
+{
+    for (int j = 3; j >= 0; j--)
+    {
+        Console.Write($"{mat[j,i]}\t");
+    }
+
+    Console.WriteLine();
+}
+
---
+++ Exercicios/ExerciciosMatrizes/Ex09/Program.cs
@@ -0,0 +1,43 @@
+﻿//Leia uma matriz 3x3.  Em seguida, solicite um número qualquer ao usuário e pesquise na matriz se o número existe.  
+//Caso, seja verdade imprima a mensagem:  “O número existe no vetor” , caso contrário “Número inexistente”.
+
+int[,] mat = new int[3, 3];
+
+for (int i = 0; i < 3; i++)
+{
+    for (int j = 0; j < 3; j++)
+    {
+        Console.Write($"Digite o valor da coluna {i + 1} e linha {j + 1}: ");
+        mat[j, i] = int.Parse(Console.ReadLine());
+    }
+    Console.WriteLine();
+}
+
+Console.Write("\nDigite o valor de pesquisa: ");
+int n = int.Parse(Console.ReadLine());
+bool pesquisa = false;
+
+for (int i = 0; i < 3; i++)
+{
+    for (int j = 0; j < 3; j++)
+    {
+        if (n == mat[j, i])
+        {
+            Console.WriteLine("O número existe no vetor");
+            pesquisa = true;
+            break;
+        }
+    }
+
+    if (i == 2)
+    {
+        Console.WriteLine("Número inexistente");
+    }
+    else if (pesquisa)
+    {
+        break;
+    }
+
+}
+
+
