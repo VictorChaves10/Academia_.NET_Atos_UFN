be3c716caf4578a858d7ceea4d3b5e9f1bfae9f2
Victor Chaves <victor.chaves460@gmail.com>
2023-04-17 18:10:06 -0300

    Ex12

---
+++ Exercicios/ExerciciosMatrizes/Ex12/Program.cs
@@ -0,0 +1,37 @@
+﻿//Escreva um programa que leia os valores de uma matriz 4x3, e em seguida mostre na tela apenas os valores cuja soma dos índices (i+j) seja um número par.
+
+int[,] mat = new int[4, 3];
+Random valueMat= new Random();
+
+for(int i = 0; i < 3; i++)
+{
+    for(int j = 0; j < 4; j++)
+    {
+        mat[j, i] = valueMat.Next(0, 20);
+    }
+}
+
+
+for (int i = 0; i < 3; i++)
+{
+    for (int j = 0; j < 4; j++)
+    {
+        Console.Write($"{mat[j, i].ToString("D2")}\t"); 
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine();
+//somente valores pares
+for (int i = 0; i < 3; i++)
+{
+    for (int j = 0; j < 4; j++)
+    {
+        if((i+j) % 2== 0)
+        {
+            Console.Write($"{mat[j, i]}  ");
+        }
+        
+    }
+}
+Console.WriteLine();
