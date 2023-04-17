89ef84fa3a1b6c0f592962c4ca3e53e2b512efc4
Victor Chaves <victor.chaves460@gmail.com>
2023-04-17 17:58:15 -0300

    Ex11

---
+++ Exercicios/ExerciciosMatrizes/Ex11/Program.cs
@@ -0,0 +1,37 @@
+﻿//Leia uma matriz A de tipo double de dimenção 3x3, crie uma nova matriz resultante da divisão dos elementos da matriz A pela soma dos seus indices.
+
+double[,] mat = new double[3, 3];
+
+for(int i = 0; i < 3; i++)
+{
+    for(int j =0; j < 3; j++)
+    {
+        Console.Write($"Digite o valor da coluna {i+1} linha {j+1}: ");
+        mat[j,i] = double.Parse(Console.ReadLine());
+    }
+    Console.WriteLine();
+}
+
+
+for (int i = 0; i < 3; i++)
+{
+    for (int j = 0; j < 3; j++)
+    {
+        Console.Write($"{mat[j,i]}\t");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine();
+
+for (int i = 0; i < 3; i++)
+{
+    double div = 0.00;
+
+    for (int j = 0; j < 3; j++)
+    {
+        div = mat[i, j] / (i + j);
+        Console.Write($"{div.ToString("F2")}\t");
+    }
+    Console.WriteLine();
+}
