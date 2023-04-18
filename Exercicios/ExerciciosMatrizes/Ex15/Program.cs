d04f32a40ebafde73091e255f570d6db0b3c5e2d
Victor Chaves <victor.chaves460@gmail.com>
2023-04-18 07:18:44 -0300

    Ex15

---
+++ Exercicios/ExerciciosMatrizes/Ex15/Program.cs
@@ -0,0 +1,51 @@
+﻿// Escreva um programa em C# para ler os valores e somar duas matrizes 4 x 4. Mostrar a matriz resultante.
+
+
+int[,] matA = new int[4, 4];
+int[,] matB = new int[4, 4];
+
+Random valueMat = new Random();
+
+for (int i = 0; i < 4; i++)
+{
+    for (int j = 0; j < 4; j++)
+    {
+        matA[j, i] = valueMat.Next(0, 20);
+        matB[j, i] = valueMat.Next(0, 20);
+
+    }
+}
+
+Console.WriteLine("\nMatriz A\n");
+for (int i = 0; i < 4; i++)
+{
+    for (int j = 0; j < 4; j++)
+    {
+        Console.Write($"{matA[i, j].ToString("D2")}\t");
+    }
+
+    Console.WriteLine();
+}
+
+Console.WriteLine("\nMatriz B\n");
+for (int i = 0; i < 4; i++)
+{
+    for (int j = 0; j < 4; j++)
+    {
+        Console.Write($"{matB[i, j].ToString("D2")}\t");
+    }
+
+    Console.WriteLine();
+}
+
+Console.WriteLine("\nSoma das Matrizes A e B\n");
+for (int i = 0; i < 4; i++)
+{
+    for (int j = 0; j < 4; j++)
+    {
+        int soma = matB[i, j] + matA[i, j];
+        Console.Write($"{soma.ToString("D2")}\t");
+    }
+
+    Console.WriteLine();
+}
