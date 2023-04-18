71f0c43b43fef01dca2a553f31bd4b0aefb6b9ed
Victor Chaves <victor.chaves460@gmail.com>
2023-04-18 14:34:42 -0300

    Ex16

--- Exercicios/ExerciciosMatrizes/Ex16/Program.cs
+++ Exercicios/ExerciciosMatrizes/Ex16/Program.cs
@@ -1,2 +1,52 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Escreva um algoritmo para transpor uma matriz 3x4 para outra 4x3. Transpor uma matriz significa transformar suas linhas em colunas e vice-versa.
+
+
+
+
+int[,] matA = new int[3, 4];
+int[,] matB = new int[4, 3];
+
+Random valueMat = new Random();
+
+
+for (int i = 0; i < 4; i++)
+{
+    for (int j = 0; j < 3; j++)
+    {
+        matA[j, i] = valueMat.Next(0, 20);
+
+    }
+}
+
+
+
+for (int i = 0; i < 3; i++)
+{
+    for (int j = 0; j < 4; j++)
+    {
+        Console.Write($"{matA[i, j].ToString("D2")}\t");
+    }
+    Console.WriteLine();
+}
+
+
+for (int i = 0; i < 3; i++)
+{
+    for (int j = 0; j <= 3; j++)
+    {
+        matB[j, i] = matA[i, j];
+    }
+}
+Console.WriteLine();
+
+for (int i = 0; i < 4; i++)
+{
+    for (int j = 0; j < 3; j++)
+    {
+        Console.Write($"{matB[i, j].ToString("D2")}\t");
+
+    }
+    Console.WriteLine();
+}
+
+
