32ab862acc1da234cbc1ead975d96b09ab2b5c8c
Victor Chaves <victor.chaves460@gmail.com>
2023-04-18 15:26:04 -0300

    Ex17

---
+++ Exercicios/ExerciciosMatrizes/Ex17/Program.cs
@@ -0,0 +1,15 @@
+﻿// Desafio: Fazer um algoritmo que leia uma matriz de 10 linhas por 10 colunas e escreva o elemento minimax, ou seja,o menor elemento da linha onde se encontra o maior elemento da matriz. Escreva também a linha e a coluna onde foi encontrado.
+
+
+int[,] mat = new int[10, 10];
+ 
+Random valueMat = new Random();
+
+
+
+
+
+static void ImprimirMatriz(int[,] mat)
+{
+
+}
