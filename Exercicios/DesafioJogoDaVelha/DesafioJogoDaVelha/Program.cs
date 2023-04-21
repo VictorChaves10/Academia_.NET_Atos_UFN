f1596672e6d9e0e7d5bb318a0c073041820241f1
Victor Chaves <victor.chaves460@gmail.com>
2023-04-21 13:16:04 -0300

    Verificação de Jogada válida
    Inseri a função que verifica se a jogada não ultrapassa o tamanho da matriz ou se o campo já está preenchido

--- Exercicios/DesafioJogoDaVelha/DesafioJogoDaVelha/Program.cs
+++ Exercicios/DesafioJogoDaVelha/DesafioJogoDaVelha/Program.cs
@@ -1,40 +1,125 @@
 ﻿
 
 
-char[,] tabuleiro = new char[3, 3];
-char player1 = 'X';
-char player2 = 'O';
-int line = 0;
-int column = 0;
+namespace JogoDaVelha
+{
+    class Program
+    {
+        static char[,] tabuleiro = new char[3, 3];
+        static int player1Victory = 0;
+        static int player2Victory = 0;
+        static int player = 1;
+        static int line = 0;
+        static int column = 0;
+    
+        static void Main(string[] args)
+        {
 
-int x = 0;
-int o = 0;
-//Tabuleiro
-Console.WriteLine("             JOGO DA VELHA\n\n");
-Console.WriteLine($"  Player1 (X) [ {x} ] x [ {o} ] Player2 (O)  \tEmpates: [ {x} ]\n\n");
-Console.WriteLine("            A       B       C");
-Console.WriteLine("\t _______________________");
-Console.WriteLine("\t|\t|\t|\t|");
-Console.WriteLine($"   0\t|   {tabuleiro[0, 0]}\t|   {tabuleiro[0, 1]}\t|   {tabuleiro[0, 2]}\t|");
-Console.WriteLine("\t|_______|_______|_______|");
-Console.WriteLine("\t|\t|\t|\t|");
-Console.WriteLine($"   1\t|   {tabuleiro[1, 0]}\t|   {tabuleiro[1, 1]}\t|   {tabuleiro[1, 2]}\t|");
-Console.WriteLine("\t|_______|_______|_______|");
-Console.WriteLine("\t|\t|\t|\t|");
-Console.WriteLine($"   2\t|   {tabuleiro[2, 0]}\t|   {tabuleiro[2, 1]}\t|   {tabuleiro[2, 2]}\t|");
-Console.WriteLine("\t|_______|_______|_______|\n");
+            do
+            {
+                Console.Clear();
+                Console.WriteLine(Tabuleiro());
+                bool jogada = false;
+
+                if (player % 2 != 0)
+                {
+                    while (jogada == false)
+                    {
+                        Console.WriteLine($"\tJogador da vez: Player 1 'X' \n");
+                        Console.Write("\tInforme a coluna desejada: ");
+                        column = int.Parse(Console.ReadLine());
+
+                        Console.Write("\tInforme a linha desejada: ");
+                        line = int.Parse(Console.ReadLine());
+                      
+                        if (JogadaValida(column, line))
+                        {
+                            tabuleiro[line, column] = 'X';
+                            jogada = true;
+                        }
+                        else
+                        {
+                            Console.Clear();
+                            Console.WriteLine(Tabuleiro());
+                            Console.WriteLine("   \t(Jogada Inválida!)\n");
+                            
+                        }
+                    }
+                }
+                else
+                {
+                    while (jogada == false)
+                    {
+                        Console.WriteLine($"\tJogador da vez: Player 2 'O' \n");
+                        Console.Write("\tInforme a coluna desejada: ");
+                        column = int.Parse(Console.ReadLine());
+
+                        Console.Write("\tInforme a linha desejada: ");
+                        line = int.Parse(Console.ReadLine());
+
+                        if (JogadaValida(column, line))
+                        {
+                            tabuleiro[line, column] = 'O';
+                            jogada = true;
+                        }
+                        else
+                        {
+                            Console.Clear();
+                            Console.WriteLine(Tabuleiro());
+                            Console.WriteLine("   \t(Jogada Inválida!)\n");
+                        }
+                    }
+
+                }
+
+                player++;
+
+            } while (player > 0);
 
 
+        }
 
-Console.Write("\tInforme a coluna desejada: ");
-column = int.Parse(Console.ReadLine());
+        public static string Tabuleiro()
+        {
+            return "             JOGO DA VELHA\n\n"
+                 + $"  Player1 (X)  [ {player1Victory} ] x [ {player2Victory} ]  Player2 ('O')  \tEmpates: [ {0} ]\n\n"
+                 + "            0       1       2\n"
+                 + "\t _______________________\n"
+                 + "\t|\t|\t|\t|\n"
+                 + $"   0\t|   {tabuleiro[0, 0]}\t|   {tabuleiro[0, 1]}\t|   {tabuleiro[0, 2]}\t|\n"
+                 + "\t|_______|_______|_______|\n"
+                 + "\t|\t|\t|\t|\n"
+                 + $"   1\t|   {tabuleiro[1, 0]}\t|   {tabuleiro[1, 1]}\t|   {tabuleiro[1, 2]}\t|\n"
+                 + "\t|_______|_______|_______|\n"
+                 + "\t|\t|\t|\t|\n"
+                 + $"   2\t|   {tabuleiro[2, 0]}\t|   {tabuleiro[2, 1]}\t|   {tabuleiro[2, 2]}\t|\n"
+                 + "\t|_______|_______|_______|\n";
+        }
 
-Console.Write("\tInforme a linha desejada: ");
-line = int.Parse(Console.ReadLine());
 
-Console.Clear();
+        public static bool JogadaValida(int column, int rowl)
+        {
+            if ((column <= 2 && column >= 0) && (rowl <= 2 && rowl >= 0))
+            {
+                if (tabuleiro[rowl, column] != '\0')
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            else
+            {
+                return false;
+            }
 
-tabuleiro[line, column] = 'X';
+
+        }
+
+    }
+}
 
 /* verificando as linhas horizontais
 
