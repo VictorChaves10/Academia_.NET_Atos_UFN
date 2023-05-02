3a0fadca6ca6817b3b9630eaa668c8c49ea14c6a
Victor Chaves <victor.chaves460@gmail.com>
2023-05-02 15:38:57 -0300

    Exercicio OO e atualização projeto

---
+++ Exercicios/ExerciciosOrientado-a-Objetos/Ex01/Carro.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex01
+{
+    internal class Carro
+    {
+        public string Marca, Modelo;
+        public int AnoFabricacao;
+
+
+
+        public void Exibe()
+        {
+            Console.WriteLine($"Marca: {Marca}, Modelo: {Modelo} e Ano de Fabricação: {AnoFabricacao}");
+        }
+
+        public override string ToString()
+        {
+            return $"Marca: {Marca}, Modelo: {Modelo} e Ano de Fabricação: {AnoFabricacao}";
+        }
+    }
+
+}    
+
---
+++ Exercicios/ExerciciosOrientado-a-Objetos/Ex01/Program.cs
@@ -0,0 +1,39 @@
+﻿
+
+//•1 - Crie uma classe chamada Carro que possua 3 atributos: marca, modelo e anoFabricacao. Além disso, na main crie 2 objetos do tipo Carro e exiba na tela os valores dos atributos criados. Esses valores devem ser solicitados ao usuário, por linha de execução.
+
+
+
+
+using Ex01;
+
+Carro car1 = new Carro();
+Carro car2 = new Carro();
+
+
+Console.Write("Digite a marca do carro : ");
+car1.Marca = Console.ReadLine();
+
+Console.Write("Digite o modelo do carro: ");
+car1.Modelo = Console.ReadLine();
+
+Console.Write("Digite o ano do carro: ");
+car1.AnoFabricacao = int.Parse(Console.ReadLine());
+
+Console.WriteLine("\nDigite os dados no outro veiculo: \n");
+
+Console.Write("Digite a marca do carro: ");
+car2.Marca = Console.ReadLine();
+
+Console.Write("Digite o modelo do carro: ");
+car2.Modelo = Console.ReadLine();
+
+Console.Write("Digite o ano do carro: ");
+car2.AnoFabricacao = int.Parse(Console.ReadLine());
+
+Console.WriteLine();
+
+Console.WriteLine(car1);
+Console.WriteLine(car2);
+
+
