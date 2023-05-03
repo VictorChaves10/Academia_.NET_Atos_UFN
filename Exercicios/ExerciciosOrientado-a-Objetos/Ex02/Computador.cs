a9c9659273cd224c4ae95765826f42d8923220a5
Victor Chaves <victor.chaves460@gmail.com>
2023-05-02 21:38:41 -0300

    Desafio HTML5 e CSS3

---
+++ Exercicios/ExerciciosOrientado-a-Objetos/Ex02/Computador.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex02
+{
+    internal class Computador
+    {
+        public string Marca, Modelo, Tipo;
+        public double Preco;
+
+        public Computador(string tipo, string marca,string modelo,  double preco)
+        {
+            Marca= marca;
+            Modelo= modelo;
+            Tipo= tipo; 
+            Preco = preco;
+
+        }
+
+        public override string ToString()
+        {
+            return $"Marca: {Marca}, Modelo: {Modelo}, Tipo: {Tipo}, Preco: R${Preco.ToString("F2")}";
+        }
+    }
+}
---
+++ Exercicios/ExerciciosOrientado-a-Objetos/Ex02/Program.cs
@@ -0,0 +1,27 @@
+﻿//Crie uma classe chamada Computador que possua 4 atributos: marca, modelo, tipo (notebook, netbook, tablet, etc) e preço. Crie dois objetos onde o primeiro objeto deverá ser criado através da solicitação dos valores ao usuário por linha de execução. Já, os valores do segundo objeto, deverão ser definidos no código-fonte da classe que o criou. Exiba na tela os resultados.
+
+
+using Ex02;
+using System.Globalization;
+
+
+Console.Write("Informe o tipo do produto: ");
+string tipo = Console.ReadLine();
+
+Console.Write("Informe a marca: ");
+string marca = Console.ReadLine();
+
+Console.Write("Informe o modelo: ");
+string modelo = Console.ReadLine();
+
+Console.Write("Informe o valor: ");
+double preco = double.Parse(Console.ReadLine());
+
+Computador p1 = new Computador(tipo,marca,modelo,preco);
+
+Computador p2 = new Computador("Video Game", "Sony", "Playstation 5", 3500.00);
+
+Console.WriteLine();
+Console.WriteLine(p1);
+Console.WriteLine();
+Console.WriteLine(p2);
