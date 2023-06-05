0a40e78f0e228b731a17a7ec9441d5fc15965efd
Victor Chaves <victor.chaves460@gmail.com>
2023-06-05 18:29:43 -0300

    Criando o projeto MVC

---
+++ Exercicios/ASP.NET/PrimeiroProjeto/PrimeiroProjeto/Controllers/HelloWorldController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace PrimeiroProjeto.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+          
+        }
+
+        public IActionResult Welcome(string nome, int numTime = 1)
+        {
+            //return "Estou no método Welcome do Hello World";
+
+            ViewData["Message"] = "Oie" + nome;
+            ViewData["NumTimes"] = numTime;
+            return View();           
+
+        }
+
+        public string Oi()
+        {
+            return "Hello";
+        }
+    }
+}
